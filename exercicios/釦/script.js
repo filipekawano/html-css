19cf4e81c274a1994d1fef817eb1753dd7c8ad97
Filipe Kawano <filipekawan@hotmail.com>
2024-07-13 21:25:42 -0300

    Botao finalizado

--- exercicios/釦/script.js
+++ exercicios/釦/script.js
@@ -1,8 +1,8 @@
 botao = document.querySelector("div.button")
+contador = 0
 
 function abaixa() {
-    // botao.style.backgroundColor = "blue";
-    botao.style.boxShadow = "inset 0px 3px 8px rgba(0, 0, 0, 0.521)";
+    botao.style.boxShadow = "inset 0px 4px 9px rgba(0, 0, 0, 0.521)";
     botao.style.transform = "translate(0, 3.5px)";
 }
 
@@ -11,6 +11,11 @@ function sobe() {
     botao.style.transform = "translate(0, -3.5px)";   
 }
 
-function mudaCor() {
-    botao.style.backgroundColor = "blue";
+function mudaCor() {   
+    if (contador % 2 == 0) {
+        botao.style.backgroundColor = "blue";
+    } else {
+        botao.style.backgroundColor = "red";      
+    }
+    contador ++    
 }
